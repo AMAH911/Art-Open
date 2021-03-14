c34bb9bf93c9af007989dac0b95151e1bae41ef2
Andrew Henry <AndrewHenry@Andrews-MacBook-Pro.local>
2021-03-14 17:32:12 -0400

    working on the client side

--- client/auth/api-auth.js
+++ client/auth/api-auth.js
@@ -1,4 +1,35 @@
 const signin = async(req, res) =>{
-    
+    try {
+        let response = await fetch('/auth/signin/', {
+            method: "POST",
+            headers: {
+                "Accept":'application/json',
+                "Content-Type":'application/json'
+            },
+            credentials: "include",
+            body:JSON.stringify(user)
+        })
+        return  await response.json()
 
+    } catch(err) {
+        console.log(err)
+
+    }
+    
+}
+
+const signout = async(req, res ) =>{
+    try { 
+        let response = await fetch('/auth/signout/', {method:'GET'})
+        return await response.json()
+
+    } catch(err){
+            console.log(err)
+    }
+
+}
+
+export  {
+    signin,
+    signout
 }
