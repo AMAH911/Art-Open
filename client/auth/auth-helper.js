a113420de77f58895a500673c91797ee7bf137a7
Andrew Henry <AndrewHenry@Andrews-MacBook-Pro.local>
2021-03-17 22:11:43 -0400

    working on the client side

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,14 @@
+import  {signout} from ".api-auth.js"
+
+const auth = {
+    isAuthenticated(){
+        if(typeof window == "undefined"){
+            
+        }
+
+
+        if(sessionStorage.getItem("jwt")){
+
+        }
+    }
+}
