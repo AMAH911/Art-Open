4bf043903c4ca4b880ab450490f6f467343074ab
Andrew Henry <AndrewHenry@Andrews-MacBook-Pro.local>
2021-03-25 14:16:51 -0400

    working on the client side

--- client/auth/Signin.js
+++ client/auth/Signin.js
@@ -34,8 +34,83 @@ const useStyles = makeStyles(theme =>({
         marginRight:theme.spacing(1),
         width: 300
 
+    },
+
+    submit: {
+        margin: 'auto',
+        marginBottom: theme.spacing(2)
     }
 
     
-})
-)
+}))
+
+ export default function Signin(){
+     const classes = useStyles()
+     const [values, setValues] = useState({
+         email:'',
+         password:"",
+         error:'',
+         redirectToReffer: false
+
+     })
+
+     const clickSubmit = ( ) =>{
+         const user = {
+             email: values.email || undefined,
+             pasword: values.passowrd || undefined
+         }
+
+         singin(user).then((data) =>{
+             if(data.error){
+                 setValues({...values, error:data.error})
+             } else {
+                 setValues({...values, error:'', redirectToUser: true})
+             }
+         })
+
+         const handleChange = name => event => {
+             setValues({...values, [name]: event.target.value})
+
+         }
+
+         const {from} = props.location.state|| {
+             from: {
+                 pathName: '/'
+             }
+         }
+
+         const {redirectToReferrer} = values
+         if(redirectToRefferer){
+             return (<Redirect to={from}/>)
+         }
+     }
+
+
+     return(
+         <Card className={classes.card}>
+             <CardContent>
+                 <Typography variant='h6' className={classes.title}>
+                    Sign in
+                 </Typography>
+                 <Textfield id="email" type="email" label="Email" className={classes.textfield} value={values.email} onChange={handleChange('email')} margin=''/><br/>
+                 <Textfield id="email" type="password" label="Password" className={classes.textfield} value={values.password} onChange={handleChange('password')} margin='normal'/>
+                 <br/>
+
+                 {
+                     values.error || (
+                         <Typography component='p' color='error'>
+                             <Icon color='error' className={classes.error}> Error </Icon>
+                         </Typography>
+                     )
+            }
+             </CardContent>
+             <CardActions>
+                 <Button color='primary' variant='contained' onClick={clickSubmit} className={classes.submit}> Submit
+
+
+                 </Button>
+             </CardActions>
+
+         </Card>
+     )
+ }
