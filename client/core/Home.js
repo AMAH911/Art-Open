e2ec7b4e6ebb4b1315de26105a5a1502233663a4
Andrew Henry <AndrewHenry@Andrews-MacBook-Pro.local>
2021-04-01 21:32:59 -0400

    working on the client side

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {makeStyles} from "@material-ui/core/styles"
 
 
 
 
 
 export default function Home(){
+
+    const classes = makeStyles(theme =>({
+        root: {
+            flexGrow: 1,
+            margin:'10px 24px'
+        }
+    }))
+
     
+
+    return(
+        <div>
+
+        </div>
+    )
+
 }
